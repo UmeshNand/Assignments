e7c9645002748564ec3faf31d6c3a628b6cc00e0
Umesh Nand <unand4@outlook.com>
2025-01-26 18:36:34 +0530

    Assignment 1 - Data Types

--- src/com/assignments/Assignment1.java
+++ src/com/assignments/Assignment1.java
@@ -4,7 +4,66 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 		
-		String name="Umesh Nand";
+		//1. Temperature of a city in degrees Celsius: 25.5 
+		float tempCity=25.5f;
+		
+		//2. Whether a customer has placed an order: true or false 
+		boolean order=false;
+		
+		//3. Person's phone number: "123-456-7890"
+		String phoneNumber="123-456-7890";
+		
+		//4. Amount of money in a customer's bank account: 1000.50
+		float amount=1000.50f;
+		
+		//5. Person's email address: "john.doe@example.com"
+		String email=" john.doe@example.com";
+		
+		//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float latitude =37.7749f; 
+		float longitude =-122.4194f; 
+		
+		//7. Person's marital status: true or false
+		boolean mStatus=false;
+		
+		//8. Person's occupation: "Software Engineer"
+		String occupation="Software Engineer";
+		
+		//9. Person's favourite colour: "Blue"
+		String fColour="Blue";
+		
+		//10. Current year: 2023 
+		String currentYear="2023";
+		
+		//11. Number of followers on a social media platform: 1,000,000 
+		int followers=1000000;
+		
+		//12. Rating of a movie: 7.5 
+		String rating="7.5";
+		
+		//13. Person's blood type: 'A'
+		char bloodType='A';
+		
+		//14. Title of a book: "To Kill a Mockingbird" 
+		String title="To Kill a Mockingbird";
+		
+		//15. Number of employees in a company: 500 
+		short nuEmployee=500;
+		
+		//16. Time of an event: 2:30 PM
+		String time="2:30 PM";
+		
+		//17. Name of a country: "United States" 
+		String country=" United States";
+		
+		//18. Person's eye color: "Brown" 
+		String eyeColour="Brown";
+		
+		//19. Person's birthplace: "New York City"
+		String birthPlace="New York City ";
+		
+		//20.Distance between two cities: 200.5 
+		float distance=200.5f;
 
 	}
 
